100a2855535120ba8ec85f6ecaa708bd60fe3468
gauravupadhya <ugaurav80@gmail.com>
2024-09-24 22:44:16 +0530

    Initial commit

--- src/CountryList.jsx
+++ src/CountryList.jsx
@@ -29,15 +29,19 @@ const CountryList = () => {
   }, []);
 
   // Filter countries based on search term
-  const filteredCountries = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredCountries = countries
+    .filter((country) =>
+      country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    // Limit results to 3 when searching for "ind"
+    .slice(0, searchTerm.toLowerCase() === 'ind' ? 3 : countries.length);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading data: {error}</p>;
 
   return (
     <div className="country-list-container">
+      {/* Input Field for Searching */}
       <input
         type="text"
         className="search-bar"
@@ -45,17 +49,25 @@ const CountryList = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+
+      {/* Country Containers */}
       <div className="country-list">
-        {filteredCountries.map((country) => (
-          <div key={country.name.common} className="country-item">
-            <img
-              src={country.flags.svg}
-              alt={`Flag of ${country.name.common}`}
-              className="country-flag"
-            />
-            <p className="country-name">{country.name.common}</p>
-          </div>
-        ))}
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((country) => (
+            <div key={country.name.common} className="country-item">
+              {country.flags && (
+                <img
+                  src={country.flags.svg}
+                  alt={`Flag of ${country.name.common}`}
+                  className="country-flag"
+                />
+              )}
+              <p className="country-name">{country.name.common}</p>
+            </div>
+          ))
+        ) : (
+          <p>No countries found</p>
+        )}
       </div>
     </div>
   );
